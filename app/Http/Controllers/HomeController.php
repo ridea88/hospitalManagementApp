f8b1b8e7e894423f6aa9e09bdecf33dfcf7eba0c
ridea88 <rideaajh88@gmail.com>
2023-08-13 14:34:36 +0700

    menampilkan data dokter di homepage user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Doctor;
 
 class HomeController extends Controller
 {
     public function redirect(){
         if(Auth::id()){
             if(Auth::user()->usertype=="0"){
-                return view("user.home");
+                $doctors = Doctor::all();
+                return view("user.home",compact('doctors'));
             }else{
                 return view('admin.home');
             }
@@ -22,6 +24,12 @@ class HomeController extends Controller
 
 
     public function index(){
-        return view('user.home');
+
+
+        if(Auth::id()){
+            return redirect('home');
+        }
+        $doctors = Doctor::all();
+        return view('user.home', compact('doctors'));
     }
 }
